db55bbeb8a8878b628b088cf5d46e7bf59c1830c
Stewart Park <jyp@epilet.com>
2015-07-31 11:35:39 -0700

    Storing into the chrome storage might crash Google Chrome if it's storing too fast.

--- chrome_localStorage.js
+++ chrome_localStorage.js
@@ -9,26 +9,34 @@
 
   chrome.storage.local.get('localStorage_data', function(data) {
     try {
-      __localStorage = JSON.parse(data.localStorage_data);
+      __localStorage = data.localStorage_data || {};
     } catch(e) {
       console.warn('Failed to load the previous data. Perhaps the first run?');
     }
   });
 
+  var timeout;
   window.__defineGetter__('localStorage', function(){
     return {
         'getItem': function(k) {
-          return __localStorage[k];
+          try {
+            return __localStorage[k];
+          } catch(e) {
+            return undefined;
+          }
         },
         'setItem': function(k, v) {
           __localStorage[k] = ''+v;
-          try {
-            chrome.storage.local.set({
-              'localStorage_data': JSON.stringify(__localStorage)
-            });
-          } catch(e) {
-            console.error(e);
-          }
+          clearTimeout(timeout);
+          timeout = setTimeout(function(){
+            try {
+              chrome.storage.local.set({
+                'localStorage_data': __localStorage
+              });
+            } catch(e) {
+              console.error(e);
+            }
+          }, 1000);
         }
     }
   });
